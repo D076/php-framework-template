0a89e01133d407e0d9ed3fdd795dec13f31345f7
Dmitry Titov <dezmond076@gmail.com>
2023-11-22 14:41:29 +0700

    Move db configs to .env

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'enabled' => false,
-    'driver' => 'mysql',
-    'host' => 'database',
-    'port' => 3306,
-    'database' => 'test',
-    'username' => 'root',
-    'password' => '',
+    'enabled' => fn() => env('DB_ENABLED', 'true'),
+    'driver' => fn() => env('DB_DRIVER', 'mysql'),
+    'host' => fn() => env('DB_HOST', 'localhost'),
+    'port' => fn() => env('DB_PORT', 3306),
+    'database' => fn() => env('DB_DATABASE', 'test'),
+    'username' => fn() => env('DB_USERNAME', 'root'),
+    'password' => fn() => env('DB_PASSWORD', ''),
     'charset' => 'utf8',
 ];
